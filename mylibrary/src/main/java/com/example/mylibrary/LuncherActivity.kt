59f5c3c8039aae1f640ae994970c4936c25badfb
anil kumar <anil.k@codebreinnovation.com>
2024-06-19 15:06:43 +0530

    initial commit 1

---
+++ mylibrary/src/main/java/com/example/mylibrary/LuncherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mylibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.librarycreater.R
+
+class LuncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_luncher)
+    }
+}
--- mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.kt
+++ mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.kt
@@ -1,6 +1,8 @@
 package com.example.mylibrary
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 
 
@@ -10,5 +12,12 @@ class ToasterMessage {
 
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+
+    }
+
+    fun loadFrame(activity: Activity)
+    {
+        activity.startActivity(Intent(activity,LuncherActivity::class.java))
+
     }
 }
